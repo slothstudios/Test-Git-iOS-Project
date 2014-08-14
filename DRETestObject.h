1cb006195782bd2024266c39d5143081737c33f1
Andre <agtacdiran@yahoo.com>
2014-08-14 15:46:19 -0700

    added a TestObject class

---
+++ DRETestObject.h
@@ -0,0 +1,13 @@
+//
+//  DRETestObject.h
+//  Git Test
+//
+//  Created by Andre Tacdiran on 8/14/14.
+//  Copyright (c) 2014 Sloth Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DRETestObject : NSObject
+
+@end
---
+++ DRETestObject.m
@@ -0,0 +1,13 @@
+//
+//  DRETestObject.m
+//  Git Test
+//
+//  Created by Andre Tacdiran on 8/14/14.
+//  Copyright (c) 2014 Sloth Studios. All rights reserved.
+//
+
+#import "DRETestObject.h"
+
+@implementation DRETestObject
+
+@end
